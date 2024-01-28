b49045e6ab55b1cd9a9121509f245e024ad4cdce
stap780 <panaet80@mail.ru>
2024-01-28 19:41:21 +0300

    change html

---
+++ app/models/marcel/spree/menu_decorator.rb
@@ -0,0 +1,37 @@
+module Marcel
+    module Spree
+      module MenuDecorator
+
+        def self.prepended(base)
+          # puts base.to_s
+          # puts base::MENU_LOCATIONS.to_s
+          location_name = 'sidebar'
+          base::MENU_LOCATIONS.push('Sidebar')
+          base::MENU_LOCATIONS_PARAMETERIZED.push(location_name)
+
+          base.define_singleton_method("for_#{location_name}") do |locale|
+            menu = find_by(location: location_name, locale: locale.to_s)
+            menu.root if menu.present?
+          end
+
+        end
+  
+        # def some_method
+        #   ...
+        # end
+  
+        protected
+  
+        # def strip_whitespaces
+        #   ...
+        # end
+
+        # def add_extra_menu_location
+        #   ...
+        # end
+
+      end
+    end
+  end
+  
+  ::Spree::Menu.prepend Marcel::Spree::MenuDecorator if ::Spree::Menu.included_modules.exclude?(Marcel::Spree::MenuDecorator)
