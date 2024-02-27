9e3e9faf9db08bb77721ba269a772aa2dbbd864b
stap780 <panaet80@mail.ru>
2024-02-27 20:50:50 +0300

    change gemfile

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Spree
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
