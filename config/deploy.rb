7f84a6fc200b53eb845d86bc7fdb6cde415fcee0
stap780 <panaet80@mail.ru>
2024-02-29 18:22:45 +0300

    change deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/#{fetch(:application)}"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle','public', 'storage', 'lib')
-set :linked_files, %w{config/database.yml config/master.key}
+set :linked_files, %w{config/database.yml config/master.key public/robots.txt public/favicon.ico public/404.html public/422.html public/500.html }
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
