2b0a47a1aea89e8bfe392c739c010919bc19ae43
stap780 <panaet80@mail.ru>
2024-03-01 13:30:15 +0300

    change cap file and deploy

--- Capfile
+++ Capfile
@@ -1,15 +1,16 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
 require 'capistrano/rails'
 require 'capistrano/bundler'
 require 'capistrano/rvm'
 
-require "capistrano/scm/git"
-install_plugin Capistrano::SCM::Git
 
 require 'capistrano/puma'
-install_plugin Capistrano::Puma
+install_plugin Capistrano::Puma, load_hooks: false
 install_plugin Capistrano::Puma::Systemd
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,12 +5,15 @@ set :repo_url,        'git@github.com:stap780/marcel.git'
 set :application,     'marcel'
 set :branch,          'main'
 # If using Digital Ocean's Ruby on Rails Marketplace framework, your username is 'rails'
+set :user, 'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+# set :use_sudo,        false
+set :use_sudo,        true
+set :sudo, "sudo -u deploy -i"
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/#{fetch(:application)}"
@@ -21,6 +24,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
