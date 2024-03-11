9542b82a0e3149e00197749f9517a31e8d1d1c45
stap780 <panaet80@mail.ru>
2024-03-11 21:21:31 +0300

    change build

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "./live_reload"
+// import "./live_reload"
 
 import * as bootstrap from "bootstrap"
 
