669ae182a3c9838e1963fdf90d296321a40b5ca4
stap780 <panaet80@mail.ru>
2024-05-21 15:13:34 +0300

    change checkout style

--- vendor/assets/javascripts/spree/frontend/custom.js
+++ vendor/assets/javascripts/spree/frontend/custom.js
@@ -1,16 +1,16 @@
 document.addEventListener("turbo:load", function() {
 
-    if ($("#checkout_form_address").length > 0 ){
-        $('[data-hook="buttons"] input').attr("class", "");
-        $('[data-hook="buttons"] input').attr("class", "btn btn-outline-dark text-uppercase font-weight-bold w-100 checkout-content-save-continue-button border-radius-0")
-    }
-    if ($("#checkout_form_delivery").length > 0 ){
-        $('[data-hook="buttons"] input').attr("class", "");
-        $('[data-hook="buttons"] input').attr("class", "btn btn-outline-dark text-uppercase font-weight-bold w-100 checkout-content-save-continue-button border-radius-0")
-    }
-    if ($("#checkout_form_payment").length > 0 ){
-        $('[data-hook="buttons"] input').attr("class", "");
-        $('[data-hook="buttons"] input').attr("class", "btn btn-outline-dark text-uppercase font-weight-bold w-100 checkout-content-save-continue-button border-radius-0")
-    }
+    // if ($("#checkout_form_address").length > 0 ){
+    //     $('[data-hook="buttons"] input').attr("class", "");
+    //     $('[data-hook="buttons"] input').attr("class", "btn btn-outline-dark text-uppercase font-weight-bold w-100 checkout-content-save-continue-button border-radius-0")
+    // }
+    // if ($("#checkout_form_delivery").length > 0 ){
+    //     $('[data-hook="buttons"] input').attr("class", "");
+    //     $('[data-hook="buttons"] input').attr("class", "btn btn-outline-dark text-uppercase font-weight-bold w-100 checkout-content-save-continue-button border-radius-0")
+    // }
+    // if ($("#checkout_form_payment").length > 0 ){
+    //     $('[data-hook="buttons"] input').attr("class", "");
+    //     $('[data-hook="buttons"] input').attr("class", "btn btn-outline-dark text-uppercase font-weight-bold w-100 checkout-content-save-continue-button border-radius-0")
+    // }
 
 })
