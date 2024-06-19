e55647f59096a5611274dd1de82b8c4ecf3e2a76
stap780 <panaet80@mail.ru>
2024-06-19 13:01:01 +0300

    add stock location decorator

---
+++ app/models/marcel/spree/stock_location_decorator.rb
@@ -0,0 +1,35 @@
+module Marcel
+    module Spree
+      module StockLocationDecorator
+
+        def self.prepended(base)
+          # puts base.to_s
+          base.before_destroy :check_associations, prepend: true
+
+        end
+  
+        # def some_method
+        #   ...
+        # end
+  
+        protected
+  
+        # def strip_whitespaces
+        #   ...
+        # end
+
+        # def add_extra_menu_location
+        #   ...
+        # end
+        def check_associations
+            if shipments.count > 0
+              errors.add(:base, "Cannot delete Stock Location. You have shipments with it.")
+              throw(:abort)
+            end
+        end
+
+      end
+    end
+  end
+  
+  ::Spree::StockLocation.prepend Marcel::Spree::StockLocationDecorator if ::Spree::StockLocation.included_modules.exclude?(Marcel::Spree::StockLocationDecorator)
