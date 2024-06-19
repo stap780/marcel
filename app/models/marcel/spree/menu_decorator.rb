3ef4d4c4dd0c02eb348a7526e92361d663c175b1
stap780 <panaet80@mail.ru>
2024-06-19 11:11:38 +0300

    add menu

--- app/models/marcel/spree/menu_decorator.rb
+++ app/models/marcel/spree/menu_decorator.rb
@@ -7,10 +7,13 @@ module Marcel
           # puts base::MENU_LOCATIONS.to_s
           location_name = 'sidebar'
           location_name2 = 'footer_small'
+          location_name3 = 'footer2'
           base::MENU_LOCATIONS.push('Sidebar')
           base::MENU_LOCATIONS.push('Footer small')
+          base::MENU_LOCATIONS.push('Footer 2')
           base::MENU_LOCATIONS_PARAMETERIZED.push(location_name)
           base::MENU_LOCATIONS_PARAMETERIZED.push(location_name2)
+          base::MENU_LOCATIONS_PARAMETERIZED.push(location_name3)
 
           base.define_singleton_method("for_#{location_name}") do |locale|
             menu = find_by(location: location_name, locale: locale.to_s)
@@ -18,6 +21,10 @@ module Marcel
           end
           base.define_singleton_method("for_#{location_name2}") do |locale|
             menu = find_by(location: location_name2, locale: locale.to_s)
+            menu.root if menu.present?
+          end
+          base.define_singleton_method("for_#{location_name3}") do |locale|
+            menu = find_by(location: location_name3, locale: locale.to_s)
             menu.root if menu.present?
           end
 
