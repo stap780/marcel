1bdb6e1aaa244224097ba22ecde544afdabc3493
stap780 <panaet80@mail.ru>
2024-07-13 08:32:23 +0300

    change gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.18.0"
+lock "~> 3.19.1"
 
 set :repo_url,        'git@github.com:stap780/marcel.git'
 set :application,     'marcel'
